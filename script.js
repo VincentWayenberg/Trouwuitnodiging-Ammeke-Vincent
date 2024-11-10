1e0d66d6e8101d495e6edfca5b44c186d7844d6c
VincentWayenberg <vincentwayenberg@hotmail.com>
2024-11-10 10:12:05 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+```javascript
+   // Script to flip the card on click
+   document.querySelector('.card').addEventListener('click', function() {
+     this.classList.toggle('flipped');
+   });
+   ```
